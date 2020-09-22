11f4a29d5a4ddff86e3d213aa8ff69f8ae54fad6
Adam Wathan <adam.wathan@gmail.com>
2020-09-22 14:22:10 -0400

    Init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-8 min-h-screen grid place-items-center">
+    <h1 class="text-7xl font-extrabold text-center">
+      <span class="bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">
+        Hello from Tailwind!
+      </span>
+    </h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  experimental: 'all',
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
